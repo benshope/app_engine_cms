72cfdd1125f45fe44accc84b8e3a8cf0aeb45905
Ben <nimajnebs@gmail.com>
2013-08-10 22:29:08 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ obj = {
     'payload': 'some var',
   } 
 self.response.out.write(json.dumps(obj))
+
+
+
+
+application = webapp.WSGIApplication(...) # Or something else if you're not using webapp
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
