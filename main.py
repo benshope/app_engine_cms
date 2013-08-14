264490ec4dcf99d4cc391e065ff1069d30542955
Ben <nimajnebs@gmail.com>
2013-08-13 17:41:56 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python
-
 import webapp2
-from boilerplate import BaseHandler
+import os
+import json
+import jinja2
 
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.api import users
 
-class MainHandler(BaseHandler):
-    def get(self):
-        self.render("index.html",{"title": "My Angular App"})
+template_dir = os.path.join(os.path.dirname(__file__), 'servertemplates')
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir), autoescape=True, variable_start_string='{{{', variable_end_string='}}}')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/view2', MainHandler),
-    ('/view3', MainHandler)
-], debug=True)
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a,**kw)
+
+    def render_str(self, template, params):
+        t = jinja_environment.get_template(template)
+
+        if self.adminCheck():
+            params['admin'] = True;
+
+        return t.render(params)
+
+    def render(self, template, kw):
+        self.write(self.render_str(template, kw))
+
+    def adminCheck(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                return True;
+            else:
+                return False
+        else:
+            return False
