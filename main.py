78c60d871c86bc268eb7e3e45a27991d440ade57
Ben <nimajnebs@gmail.com>
2013-08-13 17:48:10 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import webapp2
-import os
-import json
-import jinja2
+from boilerplate import BaseHandler
 
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.api import users
 
-template_dir = os.path.join(os.path.dirname(__file__), 'servertemplates')
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(template_dir), autoescape=True, variable_start_string='{{{', variable_end_string='}}}')
+class MainHandler(BaseHandler):
+    def get(self):
+        self.render("index.html",{"title": "My Angular App"})
 
-class BaseHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a,**kw)
-
-    def render_str(self, template, params):
-        t = jinja_environment.get_template(template)
-
-        if self.adminCheck():
-            params['admin'] = True;
-
-        return t.render(params)
-
-    def render(self, template, kw):
-        self.write(self.render_str(template, kw))
-
-    def adminCheck(self):
-        user = users.get_current_user()
-        if user:
-            if users.is_current_user_admin():
-                return True;
-            else:
-                return False
-        else:
-            return False
+app = webapp2.WSGIApplication([
+    # CLIENT SIDE PAGES (LET ANGULAR HANDLE ROUTING)
+    ('/', MainHandler),
+    ('/view2', MainHandler),
+    ('/view3', MainHandler)
+    # OTHER SERVER PAGES / ENDPOINTS
+], debug=True)
