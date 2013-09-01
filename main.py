1ee22ddea946f62c7a803475551b15dc12ce6f1b
Ben <nimajnebs@gmail.com>
2013-09-01 05:34:54 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
-from boilerplate import BaseHandler
+import os
+import json
+import jinja2
 
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.api import users
 
-class MainHandler(BaseHandler):
+template_dir = os.path.dirname(__file__)
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir), autoescape=True, variable_start_string='{{{', variable_end_string='}}}')
+
+class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a,**kw)
+
+    def render_str(self, template, params):
+        t = jinja_environment.get_template(template)
+
+        if self.adminCheck():
+            params['admin'] = True;
+
+        return t.render(params)
+
+    def render(self, template, kw):
+        self.write(self.render_str(template, kw))
+
+    def adminCheck(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                return True;
+            else:
+                return False
+        else:
+            return False
+
     def get(self):
         self.render("index.html",{"title": "My Angular App"})
+
+
 
 app = webapp2.WSGIApplication([
     # CLIENT SIDE PAGES (LET ANGULAR HANDLE ROUTING)
