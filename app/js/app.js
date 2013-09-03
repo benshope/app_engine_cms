567c906d6d99cc53c9b15b7ddbb9c958803073b5
Ben <nimajnebs@gmail.com>
2013-09-03 07:12:54 -0700

    Auto-Update

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,18 @@
 'use strict';
 
-
-// Define all the modules and their dependencies
-angular.module('myApp.filters', []);
-angular.module('myApp.services', []);
-angular.module('myApp.directives', []);
-angular.module('myApp.controllers', []);
-
-// Declare app level module which depends on filters, and services
-
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']).
   config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
 
-    $locationProvider.html5Mode(true).hashPrefix('!');
+    $locationProvider.html5Mode(true);
 
     $routeProvider.when('/', {templateUrl: 'html/partial1.html', controller: 'MainCtrl'});
     $routeProvider.when('/view2', {templateUrl: 'html/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.when('/view3', {templateUrl: 'html/partial3.html', controller: 'MyCtrl3'});
     $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+  }])  // Everything after this just allows you to scroll to a place on the destination page
+  .run(function($rootScope, $location, $anchorScroll, $routeParams) {
+    $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+      $location.hash($routeParams.scrollTo);
+      $anchorScroll();
+    });
+  });
