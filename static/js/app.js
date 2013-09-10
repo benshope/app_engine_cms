d3ce81277bc3fd94dc233f176bec5fbf772973a6
Ben <nimajnebs@gmail.com>
2013-09-10 16:38:58 -0700

    Continue constructing user login logout

--- static/js/app.js
+++ static/js/app.js
@@ -3,32 +3,44 @@
 var myApp = angular.module('myApp', ['ui.bootstrap'])
 
 myApp.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
-    $routeProvider.when('/', {templateUrl: 'static/html/home.html', controller: 'PageController'});
-    $routeProvider.when('/login', {templateUrl: 'static/html/login.html', controller: 'LoginController'});
+    $routeProvider.when('/', {templateUrl: 'html/home.html', controller: 'PageController'});
+    $routeProvider.when('/login', {templateUrl: 'html/login.html', controller: 'LoginController'});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }]);
 
-myApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
+myApp.run(function($rootScope, $location, UserService, $anchorScroll, $routeParams) {
+    var routesThatRequireUser = ['/login'];
+
+    $rootScope.$on('$routeChangeStart'), function(event, newRoute, oldRoute) {
+      // why the underscore on the line below this one?
+      if(_(routesThatRequireUser).contains($location.path()) && !UserService.isLoggedIn()) {
+        $location.path('/login')
+      }
+    }
+
     $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
       $location.hash($routeParams.scrollTo);
       $anchorScroll();
     });
+
   });
 
-myApp.controller("LoginController", function($scope, $location, AuthenticationService) {
+myApp.controller("LoginController", function($scope, $location, UserService) {
   $scope.credentials = { username: "", password: "" };
   $scope.login = function() {
-    AuthenticationService.login($scope.credentials);
+    UserService.login($scope.credentials).success( function() {
+      $location.path('/home');
+    });
   }
 });
 
-myApp.controller("PageController", function($scope, AuthenticationService) {
+myApp.controller("PageController", function($scope, UserService) {
   $scope.title = "Home";
   $scope.message = "Mouse Over these images to see a directive at work!";
 
   $scope.logout = function() {
-    AuthenticationService.logout();
+    UserService.logout();
   };
 });
 
@@ -49,13 +61,27 @@ myApp.directive("showsMessageWhenHovered", function() {
   };
 });
 
-myApp.factory("AuthenticationService", function($http, $location) {
+myApp.factory("UserService", function($http, $location) {
+  var cacheSession = function() {
+    sessionStorage.setItem('authenticated', true);
+  };
+  var unCacheSession = function() {
+    sessionStorage.removeItem('authenticated');
+  };
+
   return {
     login: function(credentials) {
-      return $http.post("/user/login", credentials);
+      var login = $http.post("/user/login", credentials);
+      login.success(cacheSession);
+      return login;
     },
     logout: function() {
-      return $http.get("/user/logout");
+      var logout = $http.get("/user/logout");
+      logout.success(unCacheSession);
+      return logout;
+    }
+    isLoggedIn: function() {
+      return sessionStorage.getItem('authenticated');
     }
   };
 });
