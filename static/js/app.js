8ae5c760d0e73d587a3a2353904020aabb70c67a
Ben <nimajnebs@gmail.com>
2013-09-11 16:02:48 -0700

    Set up controller access to the URL for fetching JSON

--- static/js/app.js
+++ static/js/app.js
@@ -3,64 +3,42 @@
 var app = angular.module('app', ['ui.bootstrap']);
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
-    $routeProvider.when('/', {templateUrl: 'html/page.html', controller: 'PageController'});
-    $routeProvider.when('/login', {templateUrl: 'html/login.html', controller: 'LoginController'});
+    $routeProvider.when('/:url', {templateUrl: 'html/page.html', controller: 'PageController'});
+    $routeProvider.when('/blog/:url', {templateUrl: 'html/post.html', controller: 'PostController'});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }]);
 
 app.run(function($rootScope, $location, User, $anchorScroll, $routeParams) {
     var routesThatRequireUser = ['/login'];
-
     $rootScope.$on('$routeChangeStart'), function(event, newRoute, oldRoute) {
       // why the underscore on the line below this one?
       if(_(routesThatRequireUser).contains($location.path()) && !User.isLoggedIn()) {
         $location.path('/login')
       }
     }
-
     $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
       $location.hash($routeParams.scrollTo);
       $anchorScroll();
     });
-
   });
 
-app.controller("LoginController", function($scope, $location, User) {
-  $scope.credentials = { username: "", password: "" };
-  $scope.login = function() {
-    User.login($scope.credentials).success( function() {
-      $location.path('/home');
-    });
-  }
-});
+app.controller("PageController", function($scope, $routeParams, User) {
+  $scope.url = $routeParams.url;
+  $scope.message = "Example directive";
 
-app.controller("PageController", function($scope, User) {
-  $scope.title = "Home";
-  $scope.message = "Mouse Over these images to see a directive at work!";
+  $scope.login = function() {
+    User.login($scope.credentials);
+  };
 
   $scope.logout = function() {
     User.logout();
   };
 });
 
-app.directive("showsMessageWhenHovered", function() {
-  return {
-    link: function(scope, element, attributes) {
-      var originalMessage = scope.message;
-      element.bind("mouseenter", function() {
-        scope.message = attributes.message;
-        scope.$apply();
-      });
-      element.bind("mouseleave", function() {
-        scope.message = originalMessage;
-        scope.$apply();
-      });
-    }
-  };
+app.controller("PostController", function($scope, $routeParams, User) {
+  $scope.url = $routeParams.url;
 });
-
-app.factory("FlashService")
 
 app.factory("User", function($http, $location) {
   return {
