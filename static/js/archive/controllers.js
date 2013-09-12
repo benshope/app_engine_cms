a7a62bea784f74815a85d538fcea61e2811c3bbb
Ben <nimajnebs@gmail.com>
2013-09-11 23:01:25 -0700

    Remove unnecessary files

--- static/js/archive/controllers.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Controllers */
-
-angular.module('myApp.controllers').
-  controller('MainCtrl', ['$scope','$timeout',function($scope, $timeout, friendService) {
-  	$scope.friends = friendService.query();
-  }]);
