ff2dc59dc6c35809e1bdaa21b19fc7beeacd9b83
Ben <nimajnebs@gmail.com>
2013-10-29 08:27:26 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import cgi
 import urllib
 from google.appengine.ext import ndb
 from google.appengine.api import users
+from google.appengine.api import mail
 
 
+# Test to set up the database correctly
 class Greeting(ndb.Model):
   """Models an individual Guestbook entry with content and date."""
   content = ndb.StringProperty()
@@ -15,7 +17,6 @@ class Greeting(ndb.Model):
   @classmethod
   def query_book(cls, ancestor_key):
     return cls.query(ancestor=ancestor_key).order(-cls.date)
-
 class MainPage(webapp2.RequestHandler):
   def get(self):
     self.response.out.write('<html><body>')
@@ -38,7 +39,6 @@ class MainPage(webapp2.RequestHandler):
         </body>
       </html>""" % (urllib.urlencode({'guestbook_name': guestbook_name}),
                     cgi.escape(guestbook_name)))
-
 class SubmitForm(webapp2.RequestHandler):
   def post(self):
     # We set the parent key on each 'Greeting' to ensure each guestbook's
@@ -49,6 +49,8 @@ class SubmitForm(webapp2.RequestHandler):
     greeting.put()
     self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
 
+
+# This class stores and retrieves pages objects from the database
 class JSON_Handler(webapp2.RequestHandler):
     def get(self, page):
         # Return the data for a url formatted as JSON
@@ -65,13 +67,26 @@ class JSON_Handler(webapp2.RequestHandler):
         # page.put()
         self.response.write('JSON Post')
 
+# This class handles users
 class User_Handler(webapp2.RequestHandler):
     def get(self, url):
         self.response.out.write('User Get')
     def post(self, url):
         self.response.out.write('User Post')
 
+# This class sends mail
+class Mail_Handler(webapp2.RequestHandler):
+  def post(self):
+        sender = 'Ben Shope <nimajnebs@gmail.com>'
+        name = 'Test Name' # self.request.get("name")
+        email = 'Ben Shope <nimajnebs@gmail.com>' # self.request.get("email")
+        subject = 'Test Message Subject'
+        body = """Test Message Body"""
+        mail.send_mail(sender, email, subject, body)
 
-# app = webapp2.WSGIApplication([('/json/(.*)', JSON_Handler), ('/user/(.*)', User_Handler)], debug=True)
+app = webapp2.WSGIApplication([
+    ('/json/(.*)', JSON_Handler), 
+    ('/user', User_Handler), 
+    ('/mail', Mail_Handler)], debug=True)
+
 # app = webapp2.WSGIApplication([('/', MainPage), ('/sign', SubmitForm)])
-app = webapp2.WSGIApplication([('/mail/(.*)', Mail_Handler)], debug=True)
