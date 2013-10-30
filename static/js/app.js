f1b7c9c4dfad9dd63864b7c832b97bb5cab9fd27
Ben <nimajnebs@gmail.com>
2013-10-29 18:14:32 -0700

    Try another post data solution

--- static/js/app.js
+++ static/js/app.js
@@ -20,42 +20,42 @@ app.controller("PageController", function($scope, $routeParams, $http) {
   $scope.message = "Example directive";
   $scope.mail_count = 0;
 
-  $scope.sendmail = function() {
+  $scope.contact = function() {
     $http.post("/mail", {'request_name': 'AT Name', 'request_body': 'AT Body'});
     $scope.mail_count += 1;
   };
 
-  $scope.getJSON = function() { };
-  $scope.postJSON = function() { };
+  // $scope.getJSON = function() { };
+  // $scope.postJSON = function() { };
 
-  $scope.login = function() {
-    User.login($scope.credentials);
-  };
-  $scope.logout = function() {
-    User.logout();
-  };
+  // $scope.login = function() {
+  //   User.login($scope.credentials);
+  // };
+  // $scope.logout = function() {
+  //   User.logout();
+  // };
 });
 
 
-app.factory("User", function($http, $location) {
-  return {
-    login: function(credentials) {
-      var login = $http.post("/user/login", credentials);
-      var login = credentials;
-      login.success(function() {
-        sessionStorage.setItem('authenticated', true);
-      });
-      return login;
-    },
-    logout: function() {
-      var logout = $http.get("/user/logout");
-      logout.success(function() {
-        sessionStorage.removeItem('authenticated');
-      });
-      return logout;
-    },
-    isLoggedIn: function() {
-      return sessionStorage.getItem('authenticated');
-    }
-  };
-});
+// app.factory("User", function($http, $location) {
+//   return {
+//     login: function(credentials) {
+//       var login = $http.post("/user/login", credentials);
+//       var login = credentials;
+//       login.success(function() {
+//         sessionStorage.setItem('authenticated', true);
+//       });
+//       return login;
+//     },
+//     logout: function() {
+//       var logout = $http.get("/user/logout");
+//       logout.success(function() {
+//         sessionStorage.removeItem('authenticated');
+//       });
+//       return logout;
+//     },
+//     isLoggedIn: function() {
+//       return sessionStorage.getItem('authenticated');
+//     }
+//   };
+// });
