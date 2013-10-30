a46a562f5b3cd765c72e776be159323ef5788485
Ben <nimajnebs@gmail.com>
2013-10-30 02:52:33 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -7,48 +7,6 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.api import mail
 
-# Test to set up the database correctly
-# class Greeting(ndb.Model):
-#   """Models an individual Guestbook entry with content and date."""
-#   content = ndb.StringProperty()
-#   date = ndb.DateTimeProperty(auto_now_add=True)
-
-#   @classmethod
-#   def query_book(cls, ancestor_key):
-#     return cls.query(ancestor=ancestor_key).order(-cls.date)
-# class MainPage(webapp2.RequestHandler):
-#   def get(self):
-#     self.response.out.write('<html><body>')
-#     guestbook_name = self.request.get('guestbook_name')
-#     ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
-#     greetings = Greeting.query_book(ancestor_key).fetch(20)
-
-#     for greeting in greetings:
-#       self.response.out.write('<blockquote>%s</blockquote>' %
-#                               cgi.escape(greeting.content))
-
-#     self.response.out.write("""
-#           <form action="/sign?%s" method="post">
-#             <div><textarea name="content" rows="3" cols="60"></textarea></div>
-#             <div><input type="submit" value="Sign Guestbook"></div>
-#           </form>
-#           <hr>
-#           <form>Guestbook name: <input value="%s" name="guestbook_name">
-#           <input type="submit" value="switch"></form>
-#         </body>
-#       </html>""" % (urllib.urlencode({'guestbook_name': guestbook_name}),
-#                     cgi.escape(guestbook_name)))
-# class SubmitForm(webapp2.RequestHandler):
-#   def post(self):
-#     # We set the parent key on each 'Greeting' to ensure each guestbook's
-#     # greetings are in the same entity group.
-#     guestbook_name = self.request.get('guestbook_name')
-#     greeting = Greeting(parent=ndb.Key("Book", guestbook_name or "*notitle*"),
-#                         content = self.request.get('content'))
-#     greeting.put()
-#     self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
-
-# This class stores and retrieves pages objects from the database
 class JSON_Handler(webapp2.RequestHandler):
     def get(self, page):
         # Return the data for a url formatted as JSON
@@ -65,25 +23,27 @@ class JSON_Handler(webapp2.RequestHandler):
         # page.put()
         self.response.write('JSON Post')
 
-# This class handles users
 class User_Handler(webapp2.RequestHandler):
     def get(self, url):
         self.response.out.write('User Get')
     def post(self, url):
         self.response.out.write('User Post')
 
-# This class sends mail from the contact form
 class Mail_Handler(webapp2.RequestHandler):
   def post(self):
-        sender = 'Ben Shope <nimajnebs@gmail.com>'
-        name = self.request.post('request_name')
-        email = 'Ben Shope <nimajnebs@gmail.com>' # self.request.get("email")
-        body = self.request.post('request_body')
-        mail.send_mail(sender, email, name, body)
+
+        print "self.request.body:  " + self.request.body
+        print "self.request.POST.get:  " + self.request.get('body')
+
+        origin = 'Ben Shope <nimajnebs@gmail.com>'
+        destination = 'Ben Shope <nimajnebs@gmail.com>'
+        email = self.request.POST.get('request_email')
+        name = self.request.POST.get('request_name')
+        message = self.request.POST.get('request_body')
+        mail.send_mail(origin, destination, name, message)
+
 
 app = webapp2.WSGIApplication([
     ('/json/(.*)', JSON_Handler), 
     ('/user', User_Handler), 
     ('/mail', Mail_Handler)], debug=True)
-
-# app = webapp2.WSGIApplication([('/', MainPage), ('/sign', SubmitForm)])
