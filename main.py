6d28354a8d9848458a01d55d80bd452a5096fa3e
Ben <nimajnebs@gmail.com>
2013-10-30 10:51:58 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -7,28 +7,6 @@ from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.api import mail
 
-class JSON_Handler(webapp2.RequestHandler):
-    def get(self, page):
-        # Return the data for a url formatted as JSON
-
-        # page_key = ndb.Key('site', url)
-        # page_data = asdfasdf.query_databse(page_key)
-        # json.dumps([page.to_dict() for page in Page.query(Page.name == url).fetch()])
-        self.response.out.write('JSON Get')
-
-    def post(self, page):
-        # Post a the data for a url to the database
-
-        # page = Page(parent=ndb.Key('site', url), content = self.request.get('content'))
-        # page.put()
-        self.response.write('JSON Post')
-
-class User_Handler(webapp2.RequestHandler):
-    def get(self, url):
-        self.response.out.write('User Get')
-    def post(self, url):
-        self.response.out.write('User Post')
-
 class Mail_Handler(webapp2.RequestHandler):
   def post(self):
         body = json.loads(self.request.body)
@@ -43,8 +21,4 @@ class Mail_Handler(webapp2.RequestHandler):
 
         mail.send_mail(sender, to, subject, message)
 
-
-app = webapp2.WSGIApplication([
-    ('/json/(.*)', JSON_Handler), 
-    ('/user', User_Handler), 
-    ('/mail', Mail_Handler)], debug=True)
+app = webapp2.WSGIApplication([('/mail', Mail_Handler)], debug=True)
