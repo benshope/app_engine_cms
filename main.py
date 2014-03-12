799de614dd0d6dfd76268095f093d2e06f51f3e4
Ben Shope <ben@benshope.com>
2014-03-12 07:49:13 -0700

    Got PathName In Angular Working

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import webapp2
 import os
-import json
 import cgi
 import urllib
+from google.appengine.ext import ndb
+from google.appengine.api import users
 from google.appengine.api import mail
 
 class Login(webapp2.RequestHandler):
-    self.redirect(users.create_login_url(self.request.uri))
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('You are logged in: ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 class Database(webapp2.RequestHandler):
-    pass
+    def get(self):
+        url = self.request.get('url')
+    def post(self):
+        url = self.request.get('url')
 
 class Contact(webapp2.RequestHandler):
     def post(self):
@@ -17,7 +27,7 @@ class Contact(webapp2.RequestHandler):
         mail.send_mail(sender=data[1], 
             to='nimajnebs@gmail.com', 
             subject='Contact Form Message', 
-            body='From: {} | {} \nMessage: {}'.format(*data))
+            body='From: {} \nEmail: {} \nMessage: {}'.format(*data))
         self.response.out.write('Thanks!  Your message has been sent.')
 
 app = webapp2.WSGIApplication([
---
+++ static/js.js
@@ -0,0 +1,43 @@
+
+var app = angular.module('app',[]);
+
+app.controller('Ctrl', function($scope, $http, $window) {
+  $scope.url = $window.location.pathname;
+
+  $scope.contact = function() {
+    $http.post('/mail', {name: 'Test Name', email: 'Test Email', message: 'Test Body'});
+  };
+
+  $scope.getJSON = function() { };
+  $scope.postJSON = function() { };
+
+  // $scope.login = function() {
+  //   User.login($scope.credentials);
+  // };
+  // $scope.logout = function() {
+  //   User.logout();
+  // };
+});
+
+// app.factory('User', function($http, $location) {
+//   return {
+//     login: function(credentials) {
+//       var login = $http.post("/user/login", credentials);
+//       var login = credentials;
+//       login.success(function() {
+//         sessionStorage.setItem('authenticated', true);
+//       });
+//       return login;
+//     },
+//     logout: function() {
+//       var logout = $http.get("/user/logout");
+//       logout.success(function() {
+//         sessionStorage.removeItem('authenticated');
+//       });
+//       return logout;
+//     },
+//     isLoggedIn: function() {
+//       return sessionStorage.getItem('authenticated');
+//     }
+//   };
+// });
