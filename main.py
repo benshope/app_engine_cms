ede2af3f6d1afb9dba3c053cc2899f1c4039ee13
Ben Shope <ben@benshope.com>
2014-03-12 05:17:40 -0700

    Add Login and Database Handlers

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import cgi
 import urllib
 from google.appengine.api import mail
 
-class SendMail(webapp2.RequestHandler):
+class Login(webapp2.RequestHandler):
+    self.redirect(users.create_login_url(self.request.uri))
+
+class Database(webapp2.RequestHandler):
+    pass
+
+class Contact(webapp2.RequestHandler):
     def post(self):
         mail.send_mail(sender="Ben Shope <nimajnebs@gmail.com>", to="Ben Shope <nimajnebs@gmail.com>", subject="BenShope Contact Form Message", body="From: {0} | {1} \nMessage: {2}".format(self.request.get('name'), self.request.get('email'), self.request.get('message')))
         self.response.out.write('Thanks!  Your message has been sent.')
 
 app = webapp2.WSGIApplication([
-  ('/mail', SendMail)
+  ('/login', Login), 
+  ('/mail', Contact), 
+  ('/database', Database)
 ])
