b78c27240d50fb7c6d5040a76f3d503b180da701
benshope <nimajnebs@gmail.com>
2014-08-12 08:02:51 -0400

    Update to Window Pods Base

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
-# © Copyright 2014 Ben Shope
-
 import webapp2
-import cgi
-import urllib
 import json
-from google.appengine.ext import ndb
-from google.appengine.api import users
 from google.appengine.api import mail
 
-class Storage(ndb.Model):
-    content  = ndb.TextProperty()
-
-class Database(webapp2.RequestHandler):
-    def get(self):
-        route = json.loads(self.request.body).get('route')
-        entity = Storage.get_by_id(route)
-        self.response.out.write(entity.content)
-    def post(self):
-        json_data = json.loads(self.request.body)
-        route, content = [json_data.get(x) for x in ['route', 'content']]
-        entity = Storage.get_or_insert(route)
-        entity.content = content
-        entity.put()
-
-class Login(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if user:
-            self.response.write('You are logged in as ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
-
-class Email(webapp2.RequestHandler):
+class SendMail(webapp2.RequestHandler):
     def post(self):
         json_data = json.loads(self.request.body)
         data = [json_data.get(x) for x in ['name', 'email', 'message']]
-        mail.send_mail(sender='nimajnebs@gmail.com', 
-            to='nimajnebs@gmail.com', 
-            subject='Contact Form Message', 
-            body='From: {} \nEmail: {} \nMessage: {}'.format(*data))
+        mail.send_mail(sender='nimajnebs@gmail.com',
+            to='nimajnebs@gmail.com',
+            subject='Window Pods',
+            body='{}\n{}\n{}'.format(*data))
         self.response.out.write('Thanks!  Your message has been sent.')
 
 app = webapp2.WSGIApplication([
-  ('/login', Login),
-  ('/email', Email),
-  ('/database', Database)
+  ('/mail', SendMail)
 ])
--- static/js/js.js
+++ static/js/js.js
@@ -1,32 +1,73 @@
 var app = angular.module('app', [ 'ui.router', 'ui.bootstrap']);
 
+app.run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+  $rootScope.$on('$stateChangeSuccess',
+    function(event){
+      if (!$window.ga)
+        return;
+      $window.ga('send', 'pageview', { page: $location.path() });
+  });
+}]);
+
 app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
   $locationProvider.html5Mode(true);
 
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: 'html/home.html'
+      templateUrl: 'html/home.html',
+      controller: 'Page'
+    })
+    .state('faq', {
+      url: '/faq',
+      templateUrl: 'html/faq.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'html/about.html'
+    })
+    .state('blog', {
+      url: '/blog',
+      templateUrl: 'html/blog.html'
     })
     .state('contact', {
       url: '/contact',
-      templateUrl: 'html/contact.html'
+      templateUrl: 'html/contact.html',
+      controller: 'Contact'
     });
 
   $urlRouterProvider.otherwise('/');
 });
 
-app.controller('PageCtrl', function($scope) { });
+app.directive('scroll', function ($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind('scroll', function() {
+            scope.scroll = Math.min($window.pageYOffset/100, 1.0);
+            scope.$apply();
+        });
+    };
+});
 
-// app.controller('ContactCtrl', function($scope, $http) {
-//   $scope.submit = function() {
-//     $http.post('/email',
-//       {name: $scope.contact_name,
-//       email: $scope.contact_email,
-//       message: $scope.contact_message});
-//     $scope.contact_sent = true;
-//   };
-// });
+app.controller('Page', function($scope, $window) {
+$scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+});
+
+app.controller('Contact', function($scope, $http) {
+  $scope.submit = function() {
+    $http.post('/mail', {
+      name: $scope.contact_name,
+      email: $scope.contact_email,
+      message: $scope.contact_message
+    });
+    $scope.contact_name = '';
+    $scope.contact_email = '';
+    $scope.contact_message = '';
+    $scope.contact_sent = true;
+  };
+});
 
 // app.directive('mapDirective', function() {
 //   return function (scope, elem, attrs) {
